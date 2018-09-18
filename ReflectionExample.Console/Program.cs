4411d55709a4bfeb200ea1ccb80cf153e0c4027d
jlopes <jean.llopes@outlook.com>
2018-09-18 01:02:57 -0300

    Program repflection

--- ReflectionExample.Console/Program.cs
+++ ReflectionExample.Console/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using System.Reflection;
 
-namespace ReflectionExample.Console
+namespace ReflectionExample
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var assembly = Assembly.GetExecutingAssembly();
+            Console.WriteLine(assembly.FullName);
+
+            var typesGetTypes = assembly.GetTypes();
+
+            foreach (var itemTypesGetTypes in typesGetTypes)
+            {
+                Console.WriteLine("Type: " + itemTypesGetTypes.Name);
+
+                var props = itemTypesGetTypes.GetProperties();
+                foreach (var itemProps in props)
+                {
+                    Console.WriteLine("\tProperty: " + itemProps.Name + " PropertyType: " + itemProps.PropertyType);
+                }
+
+
+                var fileds = itemTypesGetTypes.GetFields();
+                foreach (var itemFileds in fileds)
+                {
+                    Console.WriteLine("\tField: " + itemFileds.Name);
+                }
+
+
+                var methods = itemTypesGetTypes.GetMethods();
+                foreach (var itemMethods in methods)
+                {
+                    Console.WriteLine("\tMethod: " + itemTypesGetTypes.Name);
+                }
+
+            }
+
+
+            //EXAMPLE POPULATE
+            var sample = new Sample { NameExample = "Jean", AgeExample = 20 };
+            var sampleType = typeof(Sample);
+
+            //INVOKE YOUR METHOD BY REFLACTION
+            var sampleMethod = sampleType.GetMethod("MyMethodExample");
+            sampleMethod.Invoke(sample, null);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Sample
+    {
+        public string NameExample { get; set; }
+        public int AgeExample { get; set; }
+
+        public void MyMethodExample()
+        {
+            Console.WriteLine($"Result of method {nameof(Sample)}");
         }
     }
 }
